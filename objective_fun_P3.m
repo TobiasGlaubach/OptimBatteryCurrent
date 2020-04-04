f5962a39e28fdcb0158082a9cd8e2e016e361b54
TobiasGlaubach <tobias.glaubach@gmail.com>
2020-04-04 23:12:33 +0200

    optimized execution speed

--- objective_fun_P3.m
+++ objective_fun_P3.m
@@ -1,17 +1,17 @@
-function [ fval ] = objective_fun_P3( x, sigma_1, sigma_2, epsilon, gamma, delta, T, method, f)
+function [ fval ] = objective_fun_P3( x, sigma_1, sigma_2, epsilon, gamma, delta, T, F, f)
     
     % get I_b the only needed values from x
     I_b = x(1:T, 1);
+    
+    val_1 = epsilon * sum(penalty_function(I_b, sigma_1));
+    val_2 = (1-epsilon) * sum(penalty_function(F * I_b, sigma_2));
+    val_3 = (f * x);
+    
+    fval = gamma * (val_1 + val_2) + delta * val_3;
+end
 
-    
-    val_1 = epsilon * sum(penalty_function(I_b, sigma_1, method));
-    % HACK: there is a mistake in the paper where the sum starts at 1
-    % but if the diff is used, the sum must start at 2
-    val_2 = (1-epsilon) * sum(penalty_function(diff(I_b), sigma_2, method));
-    
-    fval = gamma * (val_1 + val_2) + delta * f * x;
-%     disp(fval)
-    return 
-    
-
+function theta = penalty_function(u, sigma)
+    theta = ones(size(u)) * inf;
+    idx = abs(u) < sigma;
+    theta(idx) = -sigma^2 * log(1-(u(idx)/sigma).^2);
 end
